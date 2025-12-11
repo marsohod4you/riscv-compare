4c890bcee5c1693a53681e1e15b572614ed80193
marsohod4you <nck.kovach@gmail.com>
2025-12-11 12:46:41 +0300

    build.sh script now takes parameter EC or IM meaning embedded/compressed with 16 regs or standard 32 regs with mul/div

--- dhrystone-fw/build.sh
+++ dhrystone-fw/build.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-#set PATH=%PATH%;d:\altera\svarka\sc-dt-2025.09-Win\sc-dt\riscv-gcc\bin
-#set PATH=%PATH%;d:\Programs\Python311\
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <OPT>"
+  echo "Where OPT is either EC (16 regs, embedded/compressed) or IM (32 regs with mul/div)"
+  exit 1
+fi
 
 export PATH=$PATH:/d/altera/syntacore/sc-dt-2025.09-Win/sc-dt/riscv-gcc/bin
 export PATH=$PATH:/d/Programs/Python311/
 
-ABI="-Wa,-march=rv32im_zicsr_zifencei -march=rv32im_zicsr_zifencei -mabi=ilp32 "
+if [ "$1" = "EC" ]; then
+	echo "BUILD FOR Embedded/Compressed code with 16 regs"
+	ABI="-Wa,-march=rv32ec_zicsr_zifencei -march=rv32ec_zicsr_zifencei -mabi=ilp32e -D__RVE_EXT -D__RVC_EXT "
+else
+	if [ "$1" = "IM" ]; then
+		echo "BUILD FOR code with mul/dev and 32 regs"
+		ABI="-Wa,-march=rv32im_zicsr_zifencei -march=rv32im_zicsr_zifencei -mabi=ilp32 "
+	else
+		echo "Please give param EC or IM"
+		exit 1
+	fi
+fi
+
 CFLAGS="-O3 "
 CFLAGS+="-funroll-loops -fpeel-loops -fgcse-sm -fgcse-las -fno-common -fno-builtin-printf "
-#CFLAGS+="-D__RVE_EXT -D__RVC_EXT -DTCM=1 "
-CFLAGS+="-D__RVC_EXT "
-#CFLAGS+="-DFLAGS_STR=\"-O3 -funroll-loops -fpeel-loops -fgcse-sm -fgcse-las \" "
 CFLAGS+="-static -std=gnu99 "
 CFLAGS+=$ABI
 CFLAGS+="-c -I./ "
-CFLAGS+="-DSELF_TIMED=1 -DTIME=1 "
+CFLAGS+="-DSELF_TIMED=1 -DTIME=1 -DTCM=1 "
 
 export CFLAGS
 
 riscv64-unknown-elf-gcc $CFLAGS start.S -o done/start.o
-#riscv64-unknown-elf-gcc $CFLAGS stdlib.c -o done/stdlib.o
 riscv64-unknown-elf-gcc $CFLAGS syscalls.c -o done/syscalls.o
 riscv64-unknown-elf-gcc $CFLAGS sc_print.c -o done/sc_print.o
 riscv64-unknown-elf-gcc $CFLAGS dhry_1.c -o done/dhry_1.o
@@ -30,4 +41,6 @@ riscv64-unknown-elf-objdump -D -D -w -x -S done/dhry.elf > done/dhry.dump
 riscv64-unknown-elf-objcopy -O verilog --verilog-data-width 1 done/dhry.elf done/dhry.hex
 python.exe mk-mif.py > done/dhry.mif
 python.exe mk-tcm-mem.py > done/dhry32.hex
-#copy done\tcm_mem.hex ..\scr1\src\top
+
+echo "Update Memory Init files of FPGA projects"
+cp done/dhry.mif ../FPGA/m3-picorv32/
