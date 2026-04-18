a93c03d0394d488a5d2a54752c7306a12abbbfba
marsohod4you <nck.kovach@gmail.com>
2026-04-18 16:05:28 +0300

    Improve FW build script to generate 2 different dirs for EC and IM configurations

--- dhrystone-fw/build.sh
+++ dhrystone-fw/build.sh
@@ -12,15 +12,21 @@ export PATH=$PATH:/d/Programs/Python311/
 if [ "$1" = "EC" ]; then
 	echo "BUILD FOR Embedded/Compressed code with 16 regs"
 	ABI="-Wa,-march=rv32ec_zicsr_zifencei -march=rv32ec_zicsr_zifencei -mabi=ilp32e -D__RVE_EXT -D__RVC_EXT "
+	DONE_DIR="./done_ec"
+	SUFFIX="_ec"
 else
 	if [ "$1" = "IM" ]; then
 		echo "BUILD FOR code with mul/dev and 32 regs"
 		ABI="-Wa,-march=rv32im_zicsr_zifencei -march=rv32im_zicsr_zifencei -mabi=ilp32 "
+		DONE_DIR="./done_im"
+		SUFFIX="_im"
 	else
 		echo "Please give param EC or IM"
 		exit 1
 	fi
 fi
+
+mkdir -p ${DONE_DIR}
 
 CFLAGS="-O3 "
 CFLAGS+="-funroll-loops -fpeel-loops -fgcse-sm -fgcse-las -fno-common -fno-builtin-printf "
@@ -31,16 +37,19 @@ CFLAGS+="-DSELF_TIMED=1 -DTIME=1 -DTCM=1 "
 
 export CFLAGS
 
-riscv64-unknown-elf-gcc $CFLAGS start.S -o done/start.o
-riscv64-unknown-elf-gcc $CFLAGS syscalls.c -o done/syscalls.o
-riscv64-unknown-elf-gcc $CFLAGS sc_print.c -o done/sc_print.o
-riscv64-unknown-elf-gcc $CFLAGS dhry_1.c -o done/dhry_1.o
-riscv64-unknown-elf-gcc $CFLAGS dhry_2.c -o done/dhry_2.o
-riscv64-unknown-elf-gcc -o done/dhry.elf -T riscv.ld done/start.o done/sc_print.o done/dhry_1.o done/dhry_2.o -nostartfiles -nostdlib -lc -lgcc --specs=nano.specs $ABI
-riscv64-unknown-elf-objdump -D -D -w -x -S done/dhry.elf > done/dhry.dump
-riscv64-unknown-elf-objcopy -O verilog --verilog-data-width 1 done/dhry.elf done/dhry.hex
-python.exe mk-mif.py > done/dhry.mif
-python.exe mk-tcm-mem.py > done/dhry32.hex
+riscv64-unknown-elf-gcc $CFLAGS start.S -o ${DONE_DIR}/start.o
+riscv64-unknown-elf-gcc $CFLAGS syscalls.c -o ${DONE_DIR}/syscalls.o
+riscv64-unknown-elf-gcc $CFLAGS sc_print.c -o ${DONE_DIR}/sc_print.o
+riscv64-unknown-elf-gcc $CFLAGS dhry_1.c -o ${DONE_DIR}/dhry_1.o
+riscv64-unknown-elf-gcc $CFLAGS dhry_2.c -o ${DONE_DIR}/dhry_2.o
+riscv64-unknown-elf-gcc -o ${DONE_DIR}/dhry.elf -T riscv.ld ${DONE_DIR}/start.o ${DONE_DIR}/sc_print.o ${DONE_DIR}/dhry_1.o ${DONE_DIR}/dhry_2.o -nostartfiles -nostdlib -lc -lgcc --specs=nano.specs $ABI
+riscv64-unknown-elf-objdump -D -D -w -x -S ${DONE_DIR}/dhry.elf > ${DONE_DIR}/dhry.dump
+riscv64-unknown-elf-objcopy -O verilog --verilog-data-width 1 ${DONE_DIR}/dhry.elf ${DONE_DIR}/dhry.hex
+python mk-mif.py > ${DONE_DIR}/dhry.mif
+python mk-tcm-mem.py > ${DONE_DIR}/dhry32.hex
 
 echo "Update Memory Init files of FPGA projects"
-cp done/dhry.mif ../FPGA/m3-picorv32/
+cp ${DONE_DIR}/dhry.mif ../FPGA/m3-picorv32/
+
+echo "Update Memory Init files for picorv32 simulations"
+cp ${DONE_DIR}/dhry.hex ../FPGA/m3-picorv32/sim/dhry${SUFFIX}.hex
