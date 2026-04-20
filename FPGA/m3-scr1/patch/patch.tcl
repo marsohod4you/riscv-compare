6cb1ff9a70ac565b9e1c40837463d8de1013c9de
marsohod4you <nck.kovach@gmail.com>
2026-04-20 17:43:58 +0300

    Improve patching SCR1 project and copying HEX CPU EC/IM codes into SCR1

--- FPGA/m3-scr1/patch/patch.tcl
+++ FPGA/m3-scr1/patch/patch.tcl
@@ -1,2 +1,4 @@
 file copy -force patch/scr1_dp_memory.sv ../../riscv-cores/scr1/src/top
-file copy ../../dhrystone-fw/done/dhry32.hex ../../riscv-cores/scr1/src/top
+file copy -force patch/scr1_csr.svh ../../riscv-cores/scr1/src/includes
+file copy -force ../../dhrystone-fw/done_ec/dhry32.hex ../../riscv-cores/scr1/src/top/dhry32_ec.hex
+file copy -force ../../dhrystone-fw/done_im/dhry32.hex ../../riscv-cores/scr1/src/top/dhry32_im.hex
