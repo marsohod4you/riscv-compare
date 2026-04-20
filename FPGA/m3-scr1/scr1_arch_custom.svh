9dd6ccb7a7c3ad3c6961b900f6e1ecab917abdf9
marsohod4you <nck.kovach@gmail.com>
2026-04-20 17:45:37 +0300

    In case of minimal CPU configuration, RVE extension, then force enabling MCOUNTEN by defining SCR1_FORCE_NEED_MCOUNTEN

--- FPGA/m3-scr1/scr1_arch_custom.svh
+++ FPGA/m3-scr1/scr1_arch_custom.svh
@@ -14,12 +14,18 @@
 //------------------------------------------------------------------------------
 // Uncomment one of these defines to set the recommended configuration:
 
+`ifdef MIN_CPU_CONFIG
+`define SCR1_CFG_RV32EC_MIN
+`define SCR1_FORCE_NEED_MCOUNTEN
+`else
 `define SCR1_CFG_RV32IMC_MAX
+`endif
+//`define SCR1_CFG_RV32IMC_MAX
 //`define SCR1_CFG_RV32IC_BASE
 //`define SCR1_CFG_RV32EC_MIN
 
-parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_RST_VECTOR        = 'h0;            // Reset vector value (start address after reset)
-parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_MTVEC_BASE        = 'h10;           // MTVEC.base field reset value, or constant value for MTVEC.base bits that are hardwired
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_RST_VECTOR        = 'h0;          // Reset vector value (start address after reset)
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_MTVEC_BASE        = 'h00000010;   // MTVEC.base field reset value, or constant value for MTVEC.base bits that are hardwired
 
 parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_MASK          = 'hFFFF0000;   // TCM mask and size
 parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_PATTERN       = 'h00000000;   // TCM address match pattern
@@ -27,6 +33,6 @@ parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_PATTERN       = 'h00000000
 parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_MASK        = 'hFFFFFFE0;   // Timer mask (should be 0xFFFFFFE0)
 parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_PATTERN     = 'h00490000;   // Timer address match pattern
   
-`define SCR1_TCM_INITIAL_DATA "tcm_data.hex"
+`define SCR1_TCM_INITIAL_DATA 1
 
 `endif // SCR1_ARCH_CUSTOM_SVH
